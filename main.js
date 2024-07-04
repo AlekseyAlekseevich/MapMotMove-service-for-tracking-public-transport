b8207d28978497d66c297bba33b1ffb0ef2dd035
mar.karnage@outlook.com <mar.karnage@outlook.com>
2024-07-04 10:24:08 +0400

    changes: main.js - input field: ListInputElement,InputElement. search and extract stop data: data_stops. filter list stops: filter_function. load data stops: ladDataInput and modificate search with InputElement.addEventListener
    style.css -
    added selectors.

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 var LOADED_STOPS = null;
+const ListInputElement = document.querySelector("#input-list");
+const InputElement = document.querySelector("#search_text");
+
+async function data_stops(name_stop)
+{
+    const data = await load_stops();
+    console.log(data);
+    let size = data.getElementsByTagName("stop").length;
+    let stop_data = data.getElementsByTagName("stop");
+    let lst = new Array();
+    console.log(stop_data[0].childNodes[1].textContent);
+    
+    for (let i = 0; i < size; i++)
+    {
+        if (stop_data[i].childNodes[1].textContent == name_stop) {
+            lst.push(data[i]);
+        }
+    }
+    
+    if (lst.size === 0)
+    {
+        lst = "No Matches";
+    }
+}
 
 async function load_stops_coord(){
     if (LOADED_STOPS)
@@ -36,3 +60,50 @@ async function get_markers() {
             .addTo(map);
     }
 }
+
+function filterFunction() {
+    var input, filter, a, i;
+    loadDataInput(LOADED_STOPS, ListInputElement);
+    input = document.getElementById("search_text");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("input-list");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+            txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+            } else {
+                a[i].style.display = "none";
+        }
+    }
+}
+
+function loadDataInput(data, element) {
+    if(data) {
+        let innerElement = "";
+        let stops_data = data.getElementsByTagName("stop");
+        for(let i = 0; i < stops_data.length; i++)
+        {
+            try
+            {
+                innerElement += `<a href="stops.html?id=${stops_data[i].getElementsByTagName("KS_ID")[0].childNodes[0].nodeValue}">` +
+                stops_data[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +"<br/> "+ 
+                stops_data[i].getElementsByTagName("direction")[0].childNodes[0].nodeValue
+                + "<br/>" + "<hr/>"+  `</a>`;
+            }
+            catch(err)
+            {
+                innerElement += `<a href="stops.html?id=${stops_data[i].getElementsByTagName("KS_ID")[0].childNodes[0].nodeValue}">` +
+                stops_data[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +"<br/> "+ `</a>`;
+            }
+        }
+        element.innerHTML = innerElement;
+    }
+}
+
+InputElement.addEventListener("input", function() {
+    if (InputElement.value === "")
+        ListInputElement.innerHTML = "";
+    else
+        filterFunction();
+});
